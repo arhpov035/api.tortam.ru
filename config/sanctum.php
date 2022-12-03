6cf1838a3fad173a5dd3bd8d21326366a97a0435
arhipov035 <arhipov035@gmail.com>
2022-12-03 18:42:14 +0300

    perenos

--- config/sanctum.php
+++ config/sanctum.php
@@ -1,7 +1,8 @@
 <?php
 
+use Laravel\Sanctum\Sanctum;
+
 return [
-    'prefix' => env('SANCTUM_PREFIX', null),
 
     /*
     |--------------------------------------------------------------------------
@@ -16,8 +17,8 @@ return [
 
     'stateful' => explode(',', env('SANCTUM_STATEFUL_DOMAINS', sprintf(
         '%s%s',
-        'localhost,tortam.ru,localhost:3000,127.0.0.1,127.0.0.1:8000,::1',
-        env('APP_URL') ? ','.parse_url(env('APP_URL'), PHP_URL_HOST) : ''
+        'localhost,localhost:3000,127.0.0.1,127.0.0.1:8000,::1',
+        Sanctum::currentApplicationUrlWithPort()
     ))),
 
     /*
